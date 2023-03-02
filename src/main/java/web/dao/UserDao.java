ac849d05120f441cafdb3283fee105df08337282
BelovaNI <belova.ni@inbox.ru>
2023-03-02 13:09:22 +0500

    02/03/2023

--- src/main/java/web/dao/UserDao.java
+++ src/main/java/web/dao/UserDao.java
@@ -1,11 +1,6 @@
 package web.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.model.Role;
 import web.model.User;
-
 import java.util.List;
-import java.util.Set;
 
 public interface UserDao {
     User getUserByName(String name);
